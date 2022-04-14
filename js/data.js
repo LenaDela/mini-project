7b36b04c82069463c76cd6f0cef97f1dc48daa22
LenaDela <lena.delalic@hotmail.com>
2022-04-14 17:19:14 +0200

    small changes to login and team pages,  plus adding API to data.js

--- js/data.js
+++ js/data.js
@@ -2,18 +2,20 @@
 const URL = "https://reqres.in/api/users?page=1";
 
 async function getDataFromApi(URL) {
-  const response = await window.fetch(URL);
+  const response = await fetch(URL); // by default it's GET
+  console.log(response)
 
   let result = await response.json();
+  console.log(result)
 
   let table = document.getElementById("teamTable");
-  let tfoot = document.getElementById("tfoot");
   let tbody = document.createElement("tbody");
-  table.insertBefore(tbody, tfoot);
-
-  let array = result.data;
+  let tfoot = document.getElementById("tfoot");
+  table.insertBefore(tbody, tfoot); // inserts tbody before tfoot in table
+  let users = result.data;
+  console.log(users)
   
-  array.map((item) => {
+  users.map((user) => {
     let tr = document.createElement("tr");
     let td = document.createElement("td");
     let td2 = document.createElement("td");
@@ -21,17 +23,17 @@ async function getDataFromApi(URL) {
     let td4 = document.createElement("td");
     let div = document.createElement("div");
     
-    let img = new Image();
-    img.src = item.avatar;
+    let img = new Image(); // It is functionally equivalent to document.createElement('img').
+    img.src = user.avatar;
     
     tbody.appendChild(tr);
-    tr?.appendChild(td);
+    tr.appendChild(td);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
-    td.innerHTML = item.last_name;
-    td2.innerHTML = item.first_name;
-    td3.innerHTML = item.email;
+    td.innerHTML = user.last_name;
+    td2.innerHTML = user.first_name;
+    td3.innerHTML = user.email;
     td4.appendChild(div);
     div.appendChild(img); 
 
@@ -76,3 +78,14 @@ async function getDataFromApi(URL) {
 
 getDataFromApi(URL);
 
+const quotesApi = "https://zenquotes.io/api/random";
+async function getQuoteApi(quotesApi) {
+  const response = await fetch(quotesApi, {
+    mode: 'no-cors'
+  }); // by default it's GET
+  console.log(response)
+
+  let result = await response.json();
+  console.log(result)
+}
+getQuoteApi(quotesApi);
--- js/login.js
+++ js/login.js
@@ -1,24 +1,20 @@
 const URL = "https://reqres.in/api/login";
 
-async function postDataApi(URL) {
-  const response = await fetch(URL, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-
-}
-// function init () {
-// const response = await fetch("https://reqres.in/api/login", {
-//   method: 'POST',
-//   body: {
-//     "email": "eve.holt@reqres.in",
-//     "password": "cityslicka"
+// async function postDataApi(URL) {
+//   const response = await fetch(URL, {
+//     method: 'POST',
+//     mode: 'cors',
+//     cache: 'no-cache',
+//     credentials: 'same-origin',
+//     headers: {
+//         'Content-Type': 'application/json'
 //     },
-// });
+//     body: JSON.stringify({
+//           "email": "eve.holt@reqres.in",
+//           "password": "cityslicka"
+//         })
+
+// }
 
 // response.json().then(data => {
 //   console.log(data);
@@ -26,3 +22,27 @@ async function postDataApi(URL) {
 // }
 
 // window.onload = init;
+//onclick
+
+const sendBtn = document.getElementById("sendBtn")
+sendBtn.addEventListener("click", login())
+
+async function login(email, password) {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // not necessary, default 
+    },
+    body: {
+      email: email, // could simply write "email". If variable has same name as key, OK
+      password: password, // ""
+    },
+  });
+  console.log(response);
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+login(URL, { answer: 42 }).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
+
